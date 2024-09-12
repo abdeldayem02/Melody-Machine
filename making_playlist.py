9a308f97f4a5be4f605675a00319e26a3962b2ba
abdeldayem02 <mohammedabdeldayem40@gmail.com>
2024-09-12 22:56:38 +0300

    updating

--- making_playlist.py
+++ making_playlist.py
@@ -14,7 +14,6 @@ secret_key = os.getenv('SECRET_KEY')
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
     client_id=api_key,
     client_secret=secret_key,
-    redirect_uri="http://localhost:8502",
     scope="playlist-modify-public user-library-read"
 ))
 
